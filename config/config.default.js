01a60140b56136c4889d50cde071c9ce7e3f66d1
caishangqing <121605366@qq.com>
2018-05-10 18:03:02 +0800

    345

--- config/config.default.js
+++ config/config.default.js
@@ -52,7 +52,13 @@ module.exports = app => {
   exports.mongoose = {
     client: {
       url: "mongodb://insurance_admin:csqcsq1214@47.52.63.21:27017/insurance",
-      options: {}
+      options: {
+        useMongoClient: true,
+        autoReconnect: true,
+        socketTimeoutMS: 0,
+        keepAlive: true,
+        reconnectTries: 30 
+      }
     }
   };
 

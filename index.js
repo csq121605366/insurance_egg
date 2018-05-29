c5b3c0b3fcf9e6b55e8f7830fc4df229d8eeda92
caishangqing <121605366@qq.com>
2018-05-29 12:22:05 +0800

    2354

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 process.env.VUE_ENV = 'server';
 require('egg').startCluster({
   baseDir: __dirname,
-  workers: process.env.WORKERS,
-  port: process.env.PORT,
+  workers: process.env.WORKERS || '2',
+  port: process.env.PORT || '7001',
   title: 'insurance',
   daemon: true
 });

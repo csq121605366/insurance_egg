8db0a9f1d9959df38f15e92bc1974248bc591ccc
caishangqing <121605366@qq.com>
2018-05-30 15:08:55 +0800

    234

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
             name: "health_happy",
             script: "./index.js",
             watch: false,
-            env: {
+            env_production: {
                 "NODE_ENV": "production",
                 "EGG_SERVER_ENV": "prod",
                 "WORKERS": 4
